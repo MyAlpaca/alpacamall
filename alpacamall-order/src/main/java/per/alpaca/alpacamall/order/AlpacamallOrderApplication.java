f1ad26876c60629b8496ef8d2029ac43e5e844a1
MyAlpaca <2789492959@qq.com>
2022-08-19 15:38:37 +0800

    测试hello,github

--- alpacamall-order/src/main/java/per/alpaca/alpacamall/order/AlpacamallOrderApplication.java
+++ alpacamall-order/src/main/java/per/alpaca/alpacamall/order/AlpacamallOrderApplication.java
@@ -8,6 +8,7 @@ public class AlpacamallOrderApplication {
 
 	public static void main(String[] args) {
 		System.out.println("hello git");
+		System.out.println("hello github");
 		SpringApplication.run(AlpacamallOrderApplication.class, args);
 	}
 
